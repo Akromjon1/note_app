045c51d51f9746f5924d771cba669299172207d7
akromjon1 <akromjon.akhmadjonov1@s.amity.edu>
2022-06-07 12:15:04 +0500

    Initial commit122

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+import 'test_page.dart';
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
   static const String id = "splash_page";
@@ -7,6 +12,17 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  _initTimer(){
+    Timer(Duration(seconds: 3),(){
+      Navigator.pushReplacementNamed(context, HomePageTwo.id);
+    });
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _initTimer();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,34 +30,32 @@ class _SplashPageState extends State<SplashPage> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Center(child: Column(
                   children: [
-                    Text("My tasks", style: TextStyle(fontSize: 32, color: Colors.white, fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+                    const Text("My tasks", style: TextStyle(fontSize: 32, color: Colors.white, fontWeight: FontWeight.bold),textAlign: TextAlign.center),
                     Image.asset("assets/icons/arabic.png")
                   ],
                 )),
 
-                SizedBox(height: 40,),
+                const SizedBox(height: 40,),
                 Center(
                   child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
                     height: MediaQuery.of(context).size.height/2.5,
                     width: MediaQuery.of(context).size.width > MediaQuery.of(context).size.height ? MediaQuery.of(context).size.width/2:MediaQuery.of(context).size.width,
-                    child: Image(
+                    child: const Image(
                       image: AssetImage("assets/icons/ic_logo.png"),
                     ),
                   ),
                 ),
                 SizedBox(height: MediaQuery.of(context).size.width > MediaQuery.of(context).size.height ?0:150),
                 Center(
-                  child: Container(
-                    child: Image(
-                      image: AssetImage("assets/icons/ic_line.png"),
-                    ),
+                  child: const Image(
+                    image: AssetImage("assets/icons/ic_line.png"),
                   ),
                 ),
                 SizedBox(height: MediaQuery.of(context).size.height/1000,),
@@ -49,7 +63,7 @@ class _SplashPageState extends State<SplashPage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text("Good", style: TextStyle(color: Colors.white.withOpacity(.5),fontSize: 15),),
-                    Text("Consistancy", style: TextStyle(color: Colors.white,fontSize: 20, fontWeight: FontWeight.bold),),
+                    const Text("Consistency", style: TextStyle(color: Colors.white,fontSize: 20, fontWeight: FontWeight.bold),),
                   ],
                 )
               ],
@@ -58,7 +72,7 @@ class _SplashPageState extends State<SplashPage> {
         ),
 
       ),
-      floatingActionButton: Image(
+      floatingActionButton: const Image(
         image: AssetImage("assets/icons/ic_cup.png"),
       ),
     );
